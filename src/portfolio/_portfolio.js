1a7aa6d3c10f7c573b1f70fe74bfbb9f7990641f
Lukasz Gawrys <lukaszgawrys13@gmail.com>
2020-03-17 22:15:13 +0100

    refreshing - portfolio start

--- src/portfolio/_portfolio.js
+++ src/portfolio/_portfolio.js
@@ -6,7 +6,58 @@ class Portfolio extends Component {
     render() {
         return (
             <div className="portfolio">
-               kjnkj
+                <div className="portfolioMain">
+                    <div className="portfolioDetailing">
+                        <div className="portfolioDetailingImg">
+                        </div>
+                        <div className="portfolioDetailingClicker">
+                            <div className="portfolioDetailingClickerBtns">
+                                <a href="#" className="detailingDemo" alt="detailingDemo">
+                                    <i className="fas fa-play"></i>
+                                </a>
+                                <a href="#" className="detailingGitHub" alt="githubToDetailing">
+                                    <i className="fab fa-github"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="portfolioFlower">
+                        <div className="portfolioFlowerImg">
+                        </div>
+                        <div className="portfolioFlowerClicker">
+                            <a href="#" className="flowerDemo" alt="flowerDemo">
+                                <i className="fas fa-play"></i>
+                            </a>
+                            <a href="#" className="flowerGitHub" alt="githubToFlower">
+                                <i className="fab fa-github"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="portfolioGame">
+                        <div className="portfolioGameImg">
+                        </div>
+                        <div className="portfolioGameClicker">
+                            <a href="#" className="gameDemo" alt="gameDemo">
+                                <i className="fas fa-play"></i>
+                            </a>
+                            <a href="#" className="gameGitHub" alt="githubToGame">
+                                <i className="fab fa-github"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="portfolioFx">
+                        <div className="portfolioFxImg">
+                        </div>
+                        <div className="portfolioFxClicker">
+                            <a href="#" className="fxDemo" alt="fxDemo">
+                                <i className="fas fa-play"></i>
+                            </a>
+                            <a href="#" className="fxGitHub" alt="githubToFx">
+                                <i className="fab fa-github"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         )
