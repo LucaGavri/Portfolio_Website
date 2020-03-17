7d56cdd66a9cff70137dd2c138f7b850ee63188f
Lukasz Gawrys <lukaszgawrys13@gmail.com>
2020-03-17 22:58:34 +0100

    refreshing - portfolio

--- src/portfolio/_portfolio.js
+++ src/portfolio/_portfolio.js
@@ -12,10 +12,10 @@ class Portfolio extends Component {
                         </div>
                         <div className="portfolioDetailingClicker">
                             <div className="portfolioDetailingClickerBtns">
-                                <a href="#" className="detailingDemo" alt="detailingDemo">
+                                <a href="https://lucagavri.github.io/WytworniaBlasku_Project/" className="detailingDemo" target="_blank">
                                     <i className="fas fa-play"></i>
                                 </a>
-                                <a href="#" className="detailingGitHub" alt="githubToDetailing">
+                                <a href="https://github.com/LucaGavri/WytworniaBlasku_Project" className="detailingGitHub" target="_blank">
                                     <i className="fab fa-github"></i>
                                 </a>
                             </div>
@@ -25,36 +25,42 @@ class Portfolio extends Component {
                         <div className="portfolioFlowerImg">
                         </div>
                         <div className="portfolioFlowerClicker">
-                            <a href="#" className="flowerDemo" alt="flowerDemo">
-                                <i className="fas fa-play"></i>
-                            </a>
-                            <a href="#" className="flowerGitHub" alt="githubToFlower">
-                                <i className="fab fa-github"></i>
-                            </a>
+                            <div className="portfolioFlowerClickerBtns">
+                                <a href="https://lucagavri.github.io/Flower_Shop_Project/" className="flowerDemo" target="_blank">
+                                    <i className="fas fa-play"></i>
+                                </a>
+                                <a href="https://github.com/LucaGavri/Flower_Shop_Project" className="flowerGitHub" target="_blank">
+                                    <i className="fab fa-github"></i>
+                                </a>
+                            </div>
                         </div>
                     </div>
                     <div className="portfolioGame">
                         <div className="portfolioGameImg">
                         </div>
                         <div className="portfolioGameClicker">
-                            <a href="#" className="gameDemo" alt="gameDemo">
-                                <i className="fas fa-play"></i>
-                            </a>
-                            <a href="#" className="gameGitHub" alt="githubToGame">
-                                <i className="fab fa-github"></i>
-                            </a>
+                            <div className="portfolioGameClickerBtns">
+                                <a href="https://lucagavri.github.io/Clicker_Evolution_Game/" className="gameDemo" target="_blank">
+                                    <i className="fas fa-play"></i>
+                                </a>
+                                <a href="https://github.com/LucaGavri/Clicker_Evolution_Game" className="gameGitHub" target="_blank">
+                                    <i className="fab fa-github"></i>
+                                </a>
+                            </div>
                         </div>
                     </div>
                     <div className="portfolioFx">
                         <div className="portfolioFxImg">
                         </div>
                         <div className="portfolioFxClicker">
-                            <a href="#" className="fxDemo" alt="fxDemo">
-                                <i className="fas fa-play"></i>
-                            </a>
-                            <a href="#" className="fxGitHub" alt="githubToFx">
-                                <i className="fab fa-github"></i>
-                            </a>
+                            <div className="portfolioFxClickerBtns">
+                                <a href="https://lucagavri.github.io/FX_Site_Project/" className="fxDemo" target="_blank">
+                                    <i className="fas fa-play"></i>
+                                </a>
+                                <a href="https://github.com/LucaGavri/FX_Site_Project" className="fxGitHub" target="_blank">
+                                    <i className="fab fa-github"></i>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
